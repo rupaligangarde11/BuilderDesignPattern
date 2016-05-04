60b9ec99e688e4465881c8208afeffd1e8f36f6c
rupaligangarde <rupaligangarde@in-rupaligangarde.local>
2016-05-04 11:47:02 +0530

    initial commit + added Pizza class

---
+++ src/Pizza.java
@@ -0,0 +1,17 @@
+public class Pizza {
+    private String dough;
+    private String toppings;
+    private String sauce;
+
+    public void setDough(String dough) {
+        this.dough = dough;
+    }
+
+    public void setToppings(String toppings) {
+        this.toppings = toppings;
+    }
+
+    public void setSauce(String sauce) {
+        this.sauce = sauce;
+    }
+}
